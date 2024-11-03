708e0ef8b62213b44bb741775292f2fcd3c1d128
Nattapong1538 <nattapong.lep@ku.th>
2024-11-03 14:16:55 +0700

    Can create Event type

---
+++ src/main/java/a4/KU_TY_backend/KU_TY_backend/entity/EventType.java
@@ -0,0 +1,26 @@
+package a4.KU_TY_backend.KU_TY_backend.entity;
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+public class EventType{
+    @Id
+    @GeneratedValue
+    private UUID typeId;
+    @Column(nullable = false, name = "type_name", unique = true)
+    private String typeName;
+    @OneToMany(mappedBy = "typeId")
+    @JsonIgnore
+    private List<Event> eventList;
+}
